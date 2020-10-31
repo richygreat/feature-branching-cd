4340ffb1a7ad1bcfbbbaa5afe03a0b265d30db4b
Richy Great <richygreat@gmail.com>
2020-10-31 06:58:58 +0100

    Spring Boot ready

---
+++ src/main/java/com/github/richygreat/fbc/FbcApplication.java
@@ -0,0 +1,11 @@
+package com.github.richygreat.fbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FbcApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/richygreat/fbc/FbcApplicationTest.java
@@ -0,0 +1,9 @@
+package com.github.richygreat.fbc;
+
+import org.junit.Test;
+
+public class FbcApplicationTest {
+    @Test
+    public void test() {
+    }
+}
